fbc94714ea365636475ce7b8a88846b0bdf8b8d4
sing-fung <1587898817@qq.com>
2022-09-08 11:27:30 -0400

    import lombok

--- src/main/java/com/singfung/tracker/model/GPS.java
+++ src/main/java/com/singfung/tracker/model/GPS.java
@@ -1,29 +1,13 @@
 package com.singfung.tracker.model;
 
+import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document(collection = "gps")
 public class GPS
 {
     private String vehicleId;
     private double lat;
     private double lng;
-
-    public String getVehicleId()
-    { return vehicleId; }
-
-    public void setVehicleId(String vehicleId)
-    { this.vehicleId = vehicleId; }
-
-    public double getLat()
-    { return lat; }
-
-    public void setLat(double lat)
-    { this.lat = lat; }
-
-    public double getLng()
-    { return lng; }
-
-    public void setLng(double lng)
-    { this.lng = lng; }
 }
